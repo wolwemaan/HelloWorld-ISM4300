a3596820fcd5eea8cc921736ce208fba8062ade4
Your Name <you@example.com>
2020-01-24 01:41:12 -0500

    Initial commit

---
+++ HelloWorld-ISM4300/Program.cs
@@ -0,0 +1,23 @@
+﻿//
+// Simple Console Program to print a message to the user and wait for key input from the user before terminating.
+// Author: Ettienne Voges
+//
+using System;
+
+namespace HelloWorld_ISM4300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Notify the user that he/she need to press any key to continue.
+            Console.WriteLine("Press any key to continue..");
+
+            // Print the text 'Hello World!'
+            Console.WriteLine("Hello World!");
+
+            // Wait for a key pressed by the user by sending true for intercept argument of ReadKey function.
+            Console.ReadKey(true);
+        }
+    }
+}
